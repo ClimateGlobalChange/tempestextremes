33ef56946e7987c820e05acb2394bc791611d6e8
Paul Ullrich <paullrich@ucdavis.edu>
2014-11-18 21:44:10 -0800

    - Added r_max_wind to header

--- DetectCyclones.cpp
+++ DetectCyclones.cpp
@@ -453,7 +453,7 @@ try {
 
 	if (fOutputHeader) {
 		fprintf(fpOutput, "#day\tmonth\tyear\tcount\thour");
-		fprintf(fpOutput, "#\t#\ti\tj\tpsl_lon\tpsl_lat\twind_max\tpsl_min");
+		fprintf(fpOutput, "#\t#\ti\tj\tpsl_lon\tpsl_lat\twind_max\tr_wind_max\tpsl_min");
 	}
 
 	// Loop through all times
