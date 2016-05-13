5a856b29586fb517ce43c4742576628a59acba09
mariellep <marielle.pinheiro@gmail.com>
2016-05-12 22:41:52 -0700

    additional blob stat code

--- test/ParseBlobStats.py
+++ test/ParseBlobStats.py
@@ -17,41 +17,10 @@ pandas.options.mode.chained_assignment = None
 #  df.loc[row] = selects by row name
 #  df.iloc[ind] = selects by row index#  
 
-
-#Bounds of colormap and the colors themselves
-#cnorm = mpl.colors.Normalize(vmin, vmax)
-#create a cmin and cmax default
-# mpl.cm.ScalarMappable(cmap=pylab.get_cmap('rainbow'))
-# c=[scalapMap.to_rgba(variable)]
-
-
 #Note: current code starts with 0016-01-01 and ends with 0016-12-31
 # For seasonal or other years, might have some issues with first December file
 # (starts on 12-07) or January file (0018 starts with 01-06)
-def BlobStats(BlobVec, plot_type, start_lon, end_lon):
-  start_lon = 0.
-  end_lon = 360.
-  
-  per_wrap = False
-  
-  if start_lon<0.:
-    start_lon += 360.
-  if end_lon<0.:
-    end_lon += 360.
-  if start_lon > end_lon:
-    per_wrap = True
-    
-def plot_all(nvec=None,svec=None,bmap=None):
-    if ((nvec != None) and (svec != None)):
-      vec = nvec + svec
-    elif (svec == None):
-      vec = nvec
-    else:
-      vec = svec
-    
-
-      
-    
+        
 ###############################################
 #f = open("/global/cscratch1/sd/marielp/2xCO2/data/y_16_stats","r")
 f = open("y_03_stats_2xCO2","r")
@@ -67,13 +36,14 @@ print("Saved indices")
 
 colnames = ['minlat','maxlat','minlon','maxlon','centlat','centlon','area']
 
+plt.figure(figsize=(24,12))
 plt.figure(1)
 #setup Basemap
 mp=Basemap(projection='cyl',llcrnrlat=-90.,urcrnrlat=90.,
             llcrnrlon=0,urcrnrlon=360.,resolution='c')
 mp.drawcoastlines()
-mp.drawparallels(numpy.arange(-90.,91.,30.))
-mp.drawmeridians(numpy.arange(0.,360.,60.))
+mp.drawparallels(numpy.arange(-90.,91.,30.),labels=[0,0,0,1])
+mp.drawmeridians(numpy.arange(0.,360.,60.),labels=[0,0,0,1])
 
 #plt.figure(2)
 #plt.figure(3)
@@ -103,25 +73,30 @@ for n in range(0,len(BlobIndices)):
     minarea=Blob.data.area.min()
   if (Blob.data.area.max()>maxarea):
     maxarea=Blob.data.area.max()
-#  if Blob.hemi == 'NH':
-#    NHVec += [Blob]
-#  else:
-#    SHVec += [Blob]
+  if Blob.hemi == 'NH':
+    NHVec += [Blob]
+  else:
+    SHVec += [Blob]
   plt.figure(1)
   Blob.map_latlon('centlon','centlat',mp)
   #plt.figure(2)
-  #Blob.plot_latlon('centlon','centlat')
+  #Blob.plot_mvmt()
   #plt.figure(3)
   #Blob.plot_area()
 
+
 #Some histograms
 #plt.figure(4)
-#plt.subplot(211)
-#init_lon = [b.data.centlon.iloc[0] for b in NHVec]
+plt.figure(3)
+plt.subplot(211)
+max_area = [b.data.area.values.max() for b in NHVec]
+dist = [b.dist for b in NHVec]
+plt.scatter(dist,max_area)
 #plt.hist(init_lon)
-#plt.subplot(212)
-#init_lon = [b.data.centlon.iloc[0] for b in SHVec]
-#plt.hist(init_lon)
+plt.subplot(212)
+max_area = [b.data.area.values.max() for b in SHVec]
+dist = [b.dist for b in SHVec]
+plt.scatter(dist,max_area)
 
 #plt.figure(5)
 #plt.subplot(211)
