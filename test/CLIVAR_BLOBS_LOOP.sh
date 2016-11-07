81f0354cdbd1813f94da533b63b2aeef5b90788e
mariellep <marielle.pinheiro@gmail.com>
2016-11-07 12:47:26 -0800

    modified blobs bash script to incorporate regional modifications

--- test/CLIVAR_BLOBS_LOOP.sh
+++ test/CLIVAR_BLOBS_LOOP.sh
@@ -7,6 +7,19 @@ DATA=("climo" "2xCO2" "SSTplus2" "SSTplus2_2xCO2")
 mstart=(3 6 9 12)
 BLOBS="FALSE"
 
+#Addition of regional parameters
+SECTOR=("NA" "NC" "NP" "SA" "SI" "SP")
+LEFT_BOUND=(250 30 130 290 20 120)
+RIGHT_BOUND=(50 150 270 40 140 310)
+MIN_LAT=(25 25 25 -75 -75 -75)
+MAX_LAT=(75 75 75 -25 -25 -25)
+
+#SECTOR=("NC" "NP" "SI" "SP")
+#LEFT_BOUND=(30 130 20 120)
+#RIGHT_BOUND=(150 260 140 310)
+#MIN_LAT=(25 25 -75 -75)
+#MAX_LAT=(75 75 -25 -25)
+
 if [ "$BLOBS" == "TRUE" ]; then
 for d in ${DATA[@]}; do
   cd $SCRATCH/$d/data
@@ -30,14 +43,23 @@ for d in ${DATA[@]}; do
         echo "ls $mstring" | sh > bloblist
       fi
       #run StitchBlobs
-      echo "$s""_""$yf"
-      blobsname="$bdir/$s""_""$yf""_blobs_""$d.nc"
-      statsname="$bdir/$s""_""$yf""_stats_""$d.txt"
-      densname="$bdir/$s""_""$yf""_dens_""$d.nc"
-      ~/tempestextremes/bin/StitchBlobs --inlist bloblist --out $blobsname --var INT_ADIPV --outvar PV_BLOB --mintime 40
-      ~/tempestextremes/bin/BlobStats --infile $blobsname --outfile $statsname --invar PV_BLOB --out minlat,maxlat,minlon,maxlon,centlat,centlon,area
-      ~/tempestextremes/bin/DensityCalculations --in $blobsname --var PV_BLOB --out $densname
-
+      #New addition! Regional stitching
+      for n in {0..5}; do
+      #for n in {0..3}; do
+        secname=${SECTOR[n]}
+        
+        echo "$s""_""$yf"
+        #blobsname="$bdir/$s""_""$yf""_blobs_""$d.nc"
+        #statsname="$bdir/$s""_""$yf""_stats_""$d.txt"
+        #densname="$bdir/$s""_""$yf""_dens_""$d.nc"
+        blobsname="$bdir/$s""_""$yf""_""$secname""_blobs_""$d.nc"
+        statsname="$bdir/$s""_""$yf""_""$secname""_stats_""$d.txt"
+        densname="$bdir/$s""_""$yf""_""$secname""_dens_""$d.nc"  
+        ~/tempestextremes/bin/StitchBlobs --inlist bloblist --out $blobsname --var INT_ADIPV --outvar PV_BLOB --mintime 40 --minlat ${MIN_LAT[n]} --maxlat ${MAX_LAT[n]} --minlon ${LEFT_BOUND[n]} --maxlon ${RIGHT_BOUND[n]}
+        ~/tempestextremes/bin/BlobStats --infile $blobsname --outfile $statsname --invar PV_BLOB --out minlat,maxlat,minlon,maxlon,centlat,centlon,area
+        ~/tempestextremes/bin/DensityCalculations --in $blobsname --var PV_BLOB --out $densname
+        n=$((n+1))
+      done
       i=$((i+1))
     done
   done
@@ -64,14 +86,14 @@ fi
 
 
 #Now run average density calculations
-for d in ${DATA[@]}; do
-  cd $SCRATCH/$d/blobs
-  for s in ${SEASONS[@]}; do
-    lsname=$s"_blobs"
-    outname=$s"_avg_dens_"$d".nc"
-    ls $s*blobs*.nc > $lsname
-    numfiles=$(cat $lsname | wc -l)
-    ~/tempestextremes/bin/DensityCalculations --std --inlist $lsname --var PV_BLOB --out $outname
-    python ~/tempestextremes/test/plot_density.py $outname "$d $numfiles yr $s" "avg" "PV blocking"
-  done
-done
+#for d in ${DATA[@]}; do
+#  cd $SCRATCH/$d/blobs
+#  for s in ${SEASONS[@]}; do
+#    lsname=$s"_blobs"
+#    outname=$s"_avg_dens_"$d".nc"
+#    ls $s*blobs*.nc > $lsname
+#    numfiles=$(cat $lsname | wc -l)
+#    ~/tempestextremes/bin/DensityCalculations --std --inlist $lsname --var PV_BLOB --out $outname
+#    python ~/tempestextremes/test/plot_density.py $outname "$d $numfiles yr $s" "avg" "PV blocking"
+#  done
+#done
