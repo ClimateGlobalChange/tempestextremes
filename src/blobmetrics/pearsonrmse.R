9a4b601ff7acaaa0c3032f51df36d7229b6181bd
mariellep <marielle.pinheiro@gmail.com>
2018-12-20 10:55:08 -0800

    added p-values to pearsonrmse output

--- src/blobmetrics/pearsonrmse.R
+++ src/blobmetrics/pearsonrmse.R
@@ -15,8 +15,49 @@ lon_convert2<-function(lon){
   return(ifelse(lon<0,360+lon,lon))
 }
 
-#Finds the Pearson pattern correlation between the averaged climatologies of the two datasets
-pearson_arr<-function(arr1,arr2,lat1,lat2,lon1,lon2,interp=FALSE,centered=FALSE){
+# #Finds the Pearson pattern correlation between the averaged climatologies of the two datasets
+# pearson_arr<-function(arr1,arr2,lat1,lat2,lon1,lon2,interp=FALSE,centered=FALSE){
+#   
+#   longdata1<-melt(arr1,value.name="V1")
+#   longdata1$lon<-lon1[longdata1$Var1]
+#   longdata1$lat<-lat1[longdata1$Var2]
+#   
+#   if (interp==TRUE){
+#     arr2_analyze<-linint(arr2,lat2,lon2,lat1,lon1)
+#   }else{
+#     arr2_analyze<-arr2
+#   }
+#   
+#   longdata2<-melt(arr2_analyze,value.name="V2")
+#   longdata2$lon<-lon1[longdata2$Var1]
+#   longdata2$lat<-lat1[longdata2$Var2]
+#   
+#   longdata<-merge(longdata1,longdata2,by=c("lon","lat"))
+#   #Create a cosine latitude column
+#   longdata$coslat<-cos(longdata$lat*pi/180)
+#   longdata$cosV1<-longdata$V1*longdata$coslat
+#   longdata$cosV2<-longdata$V2*longdata$coslat
+#   
+#   Vproduct<-longdata$cosV1*longdata$cosV2
+#   
+#   V1bar<-mean(longdata$cosV1)
+#   V2bar<-mean(longdata$cosV2)
+#   
+#   V1diff<-longdata$cosV1-V1bar
+#   V2diff<-longdata$cosV2-V2bar
+#   VdiffProduct<-V1diff*V2diff
+#   
+#   if (centered==TRUE){
+#     r<-sum(VdiffProduct)/sqrt(sum(V1diff*V1diff)*sum(V2diff*V2diff))    
+#   }else{
+#     r<-sum(Vproduct)/sqrt(sum(longdata$cosV1*longdata$cosV1)*sum(longdata$cosV2*longdata$cosV2))
+#   }
+#   
+#   return(r)
+# }
+
+pearson_permute<-function(arr1,arr2,lat1,lat2,lon1,lon2,interp=FALSE,centered=FALSE){
+  N_s<-1000
   
   longdata1<-melt(arr1,value.name="V1")
   longdata1$lon<-lon1[longdata1$Var1]
@@ -38,26 +79,38 @@ pearson_arr<-function(arr1,arr2,lat1,lat2,lon1,lon2,interp=FALSE,centered=FALSE)
   longdata$cosV1<-longdata$V1*longdata$coslat
   longdata$cosV2<-longdata$V2*longdata$coslat
   
-  Vproduct<-longdata$cosV1*longdata$cosV2
+  v1<-longdata$cosV1
+  v2<-longdata$cosV2
+  #Observed global correlation:
+  cor_obs<-as.vector(cor(v1,v2))
   
-  V1bar<-mean(longdata$cosV1)
-  V2bar<-mean(longdata$cosV2)
+  N<-nrow(longdata)
   
-  V1diff<-longdata$cosV1-V1bar
-  V2diff<-longdata$cosV2-V2bar
-  VdiffProduct<-V1diff*V2diff
-  
-  if (centered==TRUE){
-    r<-sum(VdiffProduct)/sqrt(sum(V1diff*V1diff)*sum(V2diff*V2diff))    
-  }else{
-    r<-sum(Vproduct)/sqrt(sum(longdata$cosV1*longdata$cosV1)*sum(longdata$cosV2*longdata$cosV2))
+  cvals<-c()
+  cmean<-c()
+  for (x in 1:10000){
+    #if N is too large, take a sample
+    if (N>N_s){
+      ids<-sample(1:N,N_s,replace=F)
+      longdata_sample<-longdata[ids,]
+    }else{
+      longdata_sample<-longdata
+    }
+    #Randomly permute v2
+    v1sample<-longdata_sample$cosV1
+    v2sample<-longdata_sample$cosV2
+    v2permute<-sample(v2sample,length(v2sample),replace=F)
+    cor_new<-cor(v1sample,v2permute)
+    
+    cvals[x]<-as.vector(cor_new)
+    cmean[x]<-as.vector(cor(v1sample,v2sample))
   }
-  
-  return(r)
+  pval<-mean(abs(cvals)>=abs(cor_obs))
+  vals<-list(cor.value=cor_obs,p.value=pval,c.vec=cmean)
+  return(vals)
 }
-
-#Calculates the rmse of 2 wrt 1
-rmse_calc<-function(arr1,arr2,lat1,lat2,lon1,lon2,interp=FALSE){
+rmse_permute<-function(arr1,arr2,lat1,lat2,lon1,lon2,interp=FALSE){
+  N_s<-1000
   longdata1<-melt(arr1,value.name="V1")
   longdata1$lon<-lon1[longdata1$Var1]
   longdata1$lat<-lat1[longdata1$Var2]
@@ -79,11 +132,62 @@ rmse_calc<-function(arr1,arr2,lat1,lat2,lon1,lon2,interp=FALSE){
   longdata$cosV2<-longdata$V2*longdata$coslat
   
   diff_data2<-(longdata$cosV1-longdata$cosV2)^2
-  ame<-sum(abs(longdata$cosV1-longdata$cosV2))/nrow(longdata)
-  rmse<-sqrt(sum(diff_data2)/length(diff_data2))
-  #print(sprintf("AME is %f and RMSE is %f",ame,rmse))
-  return(rmse)
+  rmse_obs<-sqrt(sum(diff_data2)/length(diff_data2))
+  rmse_perm<-c()
+  rmse_sub<-c()
+  N<-nrow(longdata)  
+  for (x in 1:10000){
+    if (N>N_s){
+      ids<-sample(1:N,N_s,replace=F)
+      longdata_sample<-longdata[ids,]
+    }else{
+      longdata_sample<-longdata
+    }
+    v1sample<-longdata_sample$cosV1
+    v2sample<-longdata_sample$cosV2
+    v2permute<-sample(v2sample,length(v2sample),replace=F)
+    diff_data2<-(v1sample-v2permute)^2
+    rmse_perm[x]<-sqrt(sum(diff_data2)/length(diff_data2))
+    diff_data2.new<-(v1sample-v2sample)^2
+    rmse_sub[x]<-sqrt(sum(diff_data2.new)/length(diff_data2.new))
+  }
+  #Is the RMSE on the left or right tail?
+  ncount<-ifelse(rmse_obs>=median(rmse_perm),sum(rmse_perm>=rmse_obs),sum(rmse_perm<=rmse_obs))
+  pval<-ncount/length(rmse_perm)
+  vals<-list(rmse.value=rmse_obs,p.value=pval,rmse.vec=rmse_sub)
+   #print(sprintf("AME is %f and RMSE is %f",ame,rmse))
+  return(vals)
 }
+
+
+# #Calculates the rmse of 2 wrt 1
+# rmse_calc<-function(arr1,arr2,lat1,lat2,lon1,lon2,interp=FALSE){
+#   longdata1<-melt(arr1,value.name="V1")
+#   longdata1$lon<-lon1[longdata1$Var1]
+#   longdata1$lat<-lat1[longdata1$Var2]
+#   
+#   if (interp==TRUE){
+#     arr2_analyze<-linint(arr2,lat2,lon2,lat1,lon1)
+#   }else{
+#     arr2_analyze<-arr2
+#   }
+#   
+#   longdata2<-melt(arr2_analyze,value.name="V2")
+#   longdata2$lon<-lon1[longdata2$Var1]
+#   longdata2$lat<-lat1[longdata2$Var2]
+#   
+#   longdata<-merge(longdata1,longdata2,by=c("lon","lat"))
+#   #Create a cosine latitude column
+#   longdata$coslat<-cos(longdata$lat*pi/180)
+#   longdata$cosV1<-longdata$V1*longdata$coslat
+#   longdata$cosV2<-longdata$V2*longdata$coslat
+#   
+#   diff_data2<-(longdata$cosV1-longdata$cosV2)^2
+#   ame<-sum(abs(longdata$cosV1-longdata$cosV2))/nrow(longdata)
+#   rmse<-sqrt(sum(diff_data2)/length(diff_data2))
+#   #print(sprintf("AME is %f and RMSE is %f",ame,rmse))
+#   return(rmse)
+# }
 
 pearson_rmse<-function(V1,V2,blobs1,time_format1,lat1,lon1,blobs2,time_format2,lat2,lon2,
                        regrid=FALSE,time_intersect=FALSE,
@@ -117,11 +221,15 @@ pearson_rmse<-function(V1,V2,blobs1,time_format1,lat1,lon1,blobs2,time_format2,l
     lon2_analyze<-round(lon2_360,3)
   }
   
-  pearson_num<-pearson_arr(b1avg,b2avg,round(lat1,3),round(lat2,3),
+  pearson_calc<-pearson_permute(b1avg,b2avg,round(lat1,3),round(lat2,3),
                            round(lon1_analyze,3),round(lon2_analyze,3),interp=regrid)
-  rmse_num<-rmse_calc(b1avg,b2avg,round(lat1,3),round(lat2,3),
+  pearson_num<-pearson_calc$cor.value
+  pearson_p<-pearson_calc$p.value
+  rmse_calc<-rmse_permute(b1avg,b2avg,round(lat1,3),round(lat2,3),
                       round(lon1_analyze,3),round(lon2_analyze,3),interp=regrid)
-  saved_names<-c("V1","V2","pearson_num","rmse_num")
+  rmse_num<-rmse_calc$rmse.value
+  rmse_p<-rmse_calc$p.value
+  saved_names<-c("V1","V2","pearson_num","rmse_num","pearson_p","rmse_p")
   if (rfn_pr!=""){
     save(list=saved_names,file=rfn_pr)
     print(sprintf("Saved %s to file",rfn_pr))
@@ -130,7 +238,9 @@ pearson_rmse<-function(V1,V2,blobs1,time_format1,lat1,lon1,blobs2,time_format2,l
     sink(txt_pr)
     cat(sprintf("Variable 1:%s \n",V1))
     cat(sprintf("Pearson correlation between average of 1 and 2: %f \n",pearson_num))
+    cat(sprintf("p-value for Pearson correlation: %f\n",pearson_p))
     cat(sprintf("RMSE between average of 1 and 2: %f \n",rmse_num))
+    cat(sprintf("p-value for RMSE: %f\n",rmse_p))
     sink()
   }
 }
