5f82064b378ff64204f0127943131cd2e7f33188
Paul Ullrich <paullrich@ucdavis.edu>
2019-07-30 10:17:50 -0700

    Add remove_depend.sh script to deal with build issues

---
+++ remove_depend.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -rf src/atmrivers/depend
+rm -rf src/base/depend
+rm -rf src/blobmetrics/depend
+rm -rf src/blobs/depend
+rm -rf src/blocking/depend
+rm -rf src/netcdf-cxx-4.2/depend
+rm -rf src/nodes/depend
